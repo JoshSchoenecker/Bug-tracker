d369fc9b45f7f5330aa32f5b774cb415a7d67959
Josh Schoenecker <schoeneckerj@gmail.com>
2020-04-27 13:27:06 -0600

    trash

--- server/services/FlawService.js
+++ server/services/FlawService.js
@@ -34,9 +34,6 @@ class FlawService{
         }
         return data;
     }
-    async closeFlaw(id, userEmail, update){
-        await dbContext.Flaws.findOneAndUpdate(id, {closed: true})
-    }
 }
 
 export const flawService = new FlawService()
